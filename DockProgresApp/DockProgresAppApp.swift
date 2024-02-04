87cf4b32b457f292eca49e9e5f753e572d9da344
Sharan Thakur <sharandt19@live.com>
2024-02-04 19:44:09 +0530

    Initial Commit

---
+++ DockProgresApp/DockProgresAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DockProgresAppApp.swift
+//  DockProgresApp
+//
+//  Created by Sharan Thakur on 04/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DockProgresAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
